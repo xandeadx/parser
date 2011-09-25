01fff8495fbf17f3a0c5ac13b0306c02e3f168b2
xandeadx <xxandeadxx@gmail.com>
2011-09-25 16:17:34 +0400

    Integrate CodeMirror for higlight php code

--- parser.js
+++ parser.js
@@ -1,26 +1,52 @@
+
 (function ($) {
-  $(function(){
-    $('input.check-button').click(function() {
-      var textarea = $(this).parent().next().find('textarea');
-      var params = {
-        test_url: $('#edit-test-url').val(),
-        field_name: textarea.attr('name'),
-        field_value: textarea.val()
-      };
+
+  Drupal.behaviors.parser = {
+    attach: function (context, settings) {
+      // Show result in popup window
+      $('input.check-button:not(.check-button-processed)', context).click(function() {
+        var $textarea = $(this).parent().next().find('textarea');
+        var params = {
+          test_url: $('#edit-test-url').val(),
+          field_name: $textarea.attr('name'),
+          field_value: $textarea.val()
+        };
       
-      $('<div id="parser-dialog">Загрузка...</div>').dialog({
-        open: function() {
-          $.post(Drupal.settings.basePath + 'parser/check', params, function(response) {
-            $('#parser-dialog').html('<pre>' + response + '</pre>');
-          });
-        },
-        close: function() {
-          $('#parser-dialog').remove();
-        },
-        title: 'Результат выполнения кода',
-        width: 800,
-        height: 400
-      });
-    });
-  });
+        $('<div id="parser-dialog">Загрузка...</div>').dialog({
+          open: function() {
+            $.post(Drupal.settings.basePath + 'parser/check', params, function(response) {
+              $('#parser-dialog').html('<pre>' + response + '</pre>');
+            });
+          },
+          close: function() {
+            $('#parser-dialog').remove();
+          },
+          title: 'Результат выполнения кода',
+          width: 800,
+          height: 400
+        });
+      }).addClass('check-button-processed');
+    
+      // CodeMirror init
+      var codeMirrors = [];
+      $('.form-textarea-wrapper:not(.resizable) > textarea:not(.codemirror-processed)', context).each(function() {
+        var i = codeMirrors.length;
+        codeMirrors[i] = CodeMirror.fromTextArea(this, {
+          mode: 'text/x-php',
+          onBlur: function() {
+            codeMirrors[i].save();
+          }
+        });
+        
+      }).addClass('codemirror-processed');
+      
+      // Refresh code mirror on fieldset collapsed
+      $('.fieldset-title:not(.codemirror-refreshed)', context).click(function() {
+        for (i in codeMirrors) {
+          codeMirrors[i].refresh();
+        }
+      }).addClass('codemirror-refreshed');
+    }
+  };
+
 }(jQuery));

2689f35f21c0b4d8b6db1f696266646f1e502d90
xandeadx <xxandeadxx@gmail.com>
2011-10-09 08:06:42 +0400

    Added opportunity manual select charset

--- parser.js
+++ parser.js
@@ -8,6 +8,7 @@
         var $textarea = $(this).parent().next().find('textarea');
         var params = {
           test_url: $('#edit-test-url').val(),
+          site_charset: $('#edit-site-charset').val(),
           field_name: $textarea.attr('name'),
           field_value: $textarea.val()
         };

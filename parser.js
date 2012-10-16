27b92f37149260d5752a0f742213fbdbeff030c2
xandeadx <xxandeadxx@gmail.com>
2012-10-16 07:58:01 +0400

    Add new options - User-Agent and Cookie

--- parser.js
+++ parser.js
@@ -12,7 +12,9 @@
           charset_fix: $('#edit-charset-fix').attr('checked') ? 1 : 0,
           field_name: $textarea.attr('name'),
           field_value: $textarea.val(),
-          list_mode_code: $('#edit-list-mode-code').val()
+          list_mode_code: $('#edit-list-mode-code').val(),
+          user_agent: $('#edit-user-agent').val(),
+          cookie: $('#edit-cookie').val()
         };
       
         $('<div id="parser-dialog">Загрузка...</div>').dialog({
